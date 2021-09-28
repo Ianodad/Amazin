386c6f2927a931711c80608407d12aecbf48fbb3
ianodad <ianodad@gmail.com>
2021-09-28 13:29:21 +0300

    working template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,30 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
+import { useEffect } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 // import '../styles/tailwind.css'
-import {wrapper} from '../redux/store';
+import { wrapper } from '../src/redux/store';
+import theme from '../src/utils/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
+};
 
 export default wrapper.withRedux(MyApp);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,9 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/core/styles';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+
+import theme from '../src/utils/theme';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -13,6 +18,11 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           {/* <link rel="apple-touch-icon" href="/apple-touch-icon"/> */}
           <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
           <meta name="theme-color" content="#fff" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         </Head>
@@ -25,4 +35,20 @@ class MyDocument extends Document {
   }
 }
 
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (WrappedComponent) => (props) => sheets.collect(<WrappedComponent {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
 export default MyDocument;
